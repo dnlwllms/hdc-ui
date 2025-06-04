bbe00aba935e183a0cdebb0c68500189dcef4dc3
가동재 <masterforce999@naver.com>
2025-06-04 22:45:37 +0900

    selection/checkbox - size / defaultVariants:sm
    확인요청
    - RDS는 Checkbox와 label을 하나의 컴포넌트로 구성 하지만 shadcn은 label과 분리해서 사용, 우리는 어느 방향으로 사용할지
    - RDS은 hover시 border색 바뀌는 부분, shadcn은 현재 미적용, 적용 여부 피드백 필요

---
+++ src/lib/components/ui/checkbox.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import type { ComponentPropsWithoutRef } from "react";
+
+import { cva, type VariantProps } from "class-variance-authority";
+import { Checkbox as CheckboxPrimitive } from "radix-ui";
+import { CheckIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+const checkboxVariants = cva(
+  "border border-gray-500 rounded-xs data-[state=checked]:bg-gray-900 data-[state=checked]:border-gray-900 data-[state=checked]:text-white",
+  {
+    variants: {
+      size: {
+        xs: "size-3.5",
+        sm: "size-4",
+      },
+    },
+    defaultVariants: {
+      size: "sm",
+    },
+  }
+);
+
+const lucideVariants = cva("", {
+  variants: {
+    size: {
+      xs: "size-3",
+      sm: "size-3.5",
+    },
+  },
+  defaultVariants: {
+    size: "sm",
+  },
+});
+
+export interface CheckboxProps
+  extends ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>,
+    VariantProps<typeof checkboxVariants> {}
+
+function Checkbox({ className, size, ...props }: CheckboxProps) {
+  return (
+    <CheckboxPrimitive.Root
+      data-slot="checkbox"
+      className={cn(checkboxVariants({ size }), "", className)}
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator
+        data-slot="checkbox-indicator"
+        className="flex items-center justify-center"
+      >
+        <CheckIcon className={lucideVariants({ size })} />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+}
+
+export { Checkbox };
