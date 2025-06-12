e16468016d0ea9f04e64e2d99d43f7b8f36be08a
가동재 <masterforce999@naver.com>
2025-06-12 11:01:29 +0900

    피드백 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,5 @@
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/lib/components/ui/tabs";
-
 function App() {
-  return (
-    <>
-      <div className="m-10 overflow-hidden">
-        <Tabs
-          defaultValue="account"
-          className="w-[1200px]"
-          mode="text"
-          size="md"
-        >
-          <TabsList className="w-full">
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
-          </TabsList>
-          <TabsContent value="account">
-            Make changes to your account here.
-          </TabsContent>
-          <TabsContent value="password">Change your password here.</TabsContent>
-        </Tabs>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
