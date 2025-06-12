bcc2c957937946abeddb00d741f1d33caae5b2b6
가동재 <masterforce999@naver.com>
2025-06-12 16:39:53 +0900

    백업

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
+import Slider from "./lib/components/ui/slider";
+
 function App() {
-  return <></>;
+  const [range, setRange] = useState<[number, number]>([10, 90]);
+  return (
+    <>
+      <div className="flex justify-center items-center size-180 m-10 bg-gray-300">
+        <Slider
+          value={range}
+          onChange={setRange}
+          hasLabel
+          labels={["10", "90"]}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
